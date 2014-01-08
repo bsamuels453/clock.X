5b6bca86709ac59e5f7cf0c391578ee5030c1a94
unknown <Ben Samuels@The_Z_Machine.(none)>
2014-01-07 19:03:33 -0800

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+
+/* features to implement
+ *
+ *
+ *
+ *
+ */
+
+int main(int argc, char** argv) {
+    for(;;){
+
+    }
+    return (EXIT_SUCCESS);
+}
