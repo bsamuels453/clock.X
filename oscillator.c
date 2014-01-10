20129848adcf6dc48c5cec66a6f7353f2cd8ebc7
Ben Samuels <bsamuels453@gmail.com>
2014-01-09 19:55:55 -0800

    adding external osc def

--- oscillator.c
+++ oscillator.c
@@ -2,8 +2,12 @@
 #include "oscillator.h"
 
 void init_osc(){
+#ifndef USE_EXT_OSC
     OSCCONbits.NOSC = 0b111; //fast rc oscillator w/ postscaler
     CLKDIVbits.RCDIV = FRCSCALE;
+#else
+    OSCCONbits.NOSC = 0b010; //ext-osc w/ xt, hs, ec
+#endif
     OSCCONbits.OSWEN = 1; //init osc switch to src specified by NOSC
     while(OSCCONbits.OSWEN != 0);
     OSCCONbits.CLKLOCK = 1;
--- oscillator.h
+++ oscillator.h
@@ -1,13 +1,17 @@
 #pragma once
 
-//used to define oscillator frequency
-//#define FRCSCALE 0b111 //31.25khz
+//#define USE_EXT_OSC
+
+#ifndef USE_EXT_OSC
+#define FRCSCALE 0b111 //31.25khz
 //#define FRCSCALE 0b110 //125khz
 //#define FRCSCALE 0b101 //250khz
 //#define FRCSCALE 0b100 //500khz
-#define FRCSCALE 0b011 //1mhz
+//#define FRCSCALE 0b011 //1mhz
 //#define FRCSCALE 0b010 //2mhz
 //#define FRCSCALE 0b001 //4mhz
 //#define FRCSCALE 0b000 //8mhz
+#else
+#endif
 
 void init_osc();
